0e28374b2a7a6c70d44487403d7e52416d9a4d6d
Hannah <hannahwoodward@hotmail.com>
2021-11-06 09:39:08 +0800

    render some images and accessed data

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
+// DOM things
+const cashSaleIMG = document.querySelector('#CashSale'), 
+creditSaleIMG = document.querySelector('#CreditSale'), 
+cashPurchaseIMG = document.querySelector('#CashPurchase'), 
+creditPurchaseIMG = document.querySelector('#CreditPurchase'), 
+salesReturnIMG = document.querySelector('#SalesReturn'), 
+purchaseReturnIMG = document.querySelector('#PurchaseReturn');
+
+// getting the data from the API
+const youTubeVideos = {}
+const topics = ["CashSale", "CreditSale", "CashPurchase", "CreditPurchase", "SalesReturn", "PurchaseReturn"]
+
 function getYouTubeData (){
-    fetch("https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLk_6zRR4T6OAz11jvyqUB-Aoay2xOK9mb&part=snippet&maxResults=50", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
-            "x-rapidapi-key": "e99e6ff537msh256aa6ba07c6c09p17565cjsn2e73e7783a62"
-        }
-    })
+    fetch("https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLk_6zRR4T6OAz11jvyqUB-Aoay2xOK9mb&part=snippet&maxResults=6", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+		"x-rapidapi-key": "e99e6ff537msh256aa6ba07c6c09p17565cjsn2e73e7783a62"
+	}
+})
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        let video = document.querySelector('#youTubeVideo');
-        let videoLink = data.items[0].snippet.resourceId.videoId
-        console.log(videoLink)
-        video.src= `https://www.youtube.com/embed/${videoLink}`
-        //https://www.youtube.com/watch?v=VK40REVJAzM
+        //console.log(data)
+        topics.forEach(topic => {
+            youTubeVideos[topic] = findSomething(topic, data.items)
+            renderImg()
+        })
     })
     .catch(error => console.error(error)
     )};
 
-getYouTubeData();
+getYouTubeData() 
 
-// thumbnail rendering
-let thumbnail = document.querySelectorAll('.thumbnail')
+function findSomething(topic, arr) {
+    return arr.find(item => item.snippet.title === topic)
+};
 
-function displayThumbnail () {
-    thumbnail.src=data.items[0].thumbnails.high;
-}
-thumbnail.src=
+console.log(youTubeVideos)
 
+//let picURL = youTubeVideos[CashSale][snippet][thumbnails][high][url]
+//cashSaleIMG.src = picURL
 
-// Get the modal
-let modal1 = document.querySelector("#myModal");
-
-// Get the button that opens the modal
-let btn = document.querySelector("#CS");
-
-// Get the <span> element that closes the modal
-const span = document.querySelector(".close");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal1.style.display = "block";
+function renderImg() {
+    let picURL = youTubeVideos.CashSale.snippet.thumbnails.high.url
+    cashSaleIMG.src = picURL
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+/*class Transaction {
+    constructor(name) {
+        this.name=name;
+    }
+    renderImg () {
+        let picURL = youTubeVideos.${this.name}.snippet.thumbnails.high.url
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+    }
+}*/
+
+// modal 
+// modal element 
+const modal = document.getElementById('myModal'), 
+closeBtn = document.getElementsByClassName('close')[0],
+iframe = document.querySelector('iframe');
+
+//listen for a open click
+cashSaleIMG.addEventListener('click', openModal);
+//listen for close click
+closeBtn.addEventListener('click', closeModal);
+//listen for outside click
+window.addEventListener('click', clickOutside);
+
+//function to open the modal
+function openModal() {
+    modal.style.display = 'block';
+    iframe.src = 'https://www.youtube.com/embed/gHrNmXh5reg?autoplay=1&mute=1'
+}
+//function to close the modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+function clickOutside(e) {
+    if(e.target == modal){
+        modal.style.display = 'none'
+    }
+}
